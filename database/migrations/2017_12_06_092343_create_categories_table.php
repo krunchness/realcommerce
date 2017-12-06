73a7682884893000cf484fca3e44f6d46963bcb4
=krunchness <=enjay.navarro@gmail.com>
2017-12-06 17:33:30 +0800

    Carbon and categories

---
+++ database/migrations/2017_12_06_092343_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('parent')->nullable();
+            $table->timestamps();
+        });
+
+        DB::table('categories')->insert([
+                'name' => 'Uncategorized',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
